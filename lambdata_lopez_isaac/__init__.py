36f96df9da6ba7a5f160293f95076a73ea3033e4
lopez-isaac <lopez-isaac@users.noreply.github.com>
2019-12-02 16:52:01 -0500

    added init file with datasets and functions

---
+++ lambdata_lopez_isaac/__init__.py
@@ -0,0 +1,14 @@
+""" lambdata - a collection of data science helper functions """
+
+import numpy as np
+import pandas as pd
+
+#sample code
+
+#sample dataset
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(10))
+
+#simple function
+def increment(x):
+    return(x + 1)
